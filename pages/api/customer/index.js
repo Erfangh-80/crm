3ef6787fe275ded6e6aaa56a1159b0ed2b96113b
Erfangh-80 <72462171+Erfangh-80@users.noreply.github.com>
2023-04-04 12:46:26 +0430

    create post to db api in api route

---
+++ pages/api/customer/index.js
@@ -0,0 +1,33 @@
+import Customer from "@/model/Customer";
+import connectDB from "@/utils/connectDB";
+
+export default async function handler(req, res) {
+  try {
+    await connectDB();
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ status: "failed", message: "error in connection to db" });
+  }
+
+  if (req.method === "POST") {
+    const data = req.body.data;
+    if (!data.name || !data.lastName || !data.email)
+      return res
+        .status(400)
+        .json({ status: "failed", message: "Invalid data" });
+
+    try {
+      const customer = await Customer.create(data);
+      res
+        .status(201)
+        .json({ status: "success", message: "data created", data: customer });
+    } catch (err) {
+      console.log(err);
+      return res
+        .status(500)
+        .json({ status: "failed", message: "error in storing data in db" });
+    }
+  }
+}
