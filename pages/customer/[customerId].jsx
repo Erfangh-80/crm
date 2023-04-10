987ab35db3b3a4db14fd8cac0186bfbb7ce85790
Erfangh-80 <72462171+Erfangh-80@users.noreply.github.com>
2023-04-10 23:33:19 +0430

    create get data customer component

---
+++ pages/customer/[customerId].jsx
@@ -0,0 +1,26 @@
+import CustomerDetail from "@/components/template/CustomerDetail";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const customerDetail = () => {
+  const [data, setData] = useState(null);
+
+  const router = useRouter();
+  const {
+    query: { customerId },
+    isReady,
+  } = router;
+
+  useEffect(() => {
+    if (isReady)
+      axios
+        .get(`/api/customer/${customerId}`)
+        .then((response) => setData(response.data))
+        .catch((err) => console.log(err));
+  }, [isReady]);
+
+  if (data) return <CustomerDetail data={data.data}/>;
+};
+
+export default customerDetail;
